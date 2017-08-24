94459e31d83e02f7ad1f7a5ce0a2ac09b5ef2faa
Riley Baxter <rbrileybaxter@gmail.com>
2017-08-23 20:05:08 -0400

    Add import of absolute_import and division from __future__

--- apt_medium/apt_medium.py
+++ apt_medium/apt_medium.py
@@ -18,7 +18,7 @@
     Copyright (c) 2017 Riley Baxter
 """
 
-from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import argparse
 import distutils.spawn
