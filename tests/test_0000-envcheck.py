11d3d3ee16e007faa499f0d6119575046f0e8226
Riley Baxter <rbrileybaxter@gmail.com>
2017-09-14 17:07:49 -0400

    Make pytest fail during the test collection phase if we aren't root.

--- tests/test_0000-envcheck.py
+++ tests/test_0000-envcheck.py
@@ -1,7 +1,9 @@
 from apt_medium.apt_medium import check_sysreqs
 import os
 
+# Prevent pytest from running if we are not root
+if os.geteuid() != 0:
+    raise Exception("Tests must be run as root")
+
 def test_sysreqs():
-    if os.geteuid() != 0:
-        raise Exception("Tests must be run as root")
     check_sysreqs()
