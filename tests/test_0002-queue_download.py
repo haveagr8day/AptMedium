b9da125e14cca2ecb22fdc24d31bb710459ec39a
Riley Baxter <rbrileybaxter@gmail.com>
2021-03-30 21:58:37 -0400

    Add test for queuing downloads on different system

--- tests/test_0002-queue_download.py
+++ tests/test_0002-queue_download.py
@@ -11,6 +11,7 @@ except ImportError:
 
 simplepkg = 'fonts-3270'
 complexpkg = 'wireshark'
+bogushostname = 'somethingelse'
 
 # Test queuing package with no dependencies
 def test_queue_direct(capsys, monkeypatch, hostname):
@@ -25,6 +26,24 @@ def test_queue_direct(capsys, monkeypatch, hostname):
         assert re.match("Queued fonts-3270 for download", output[2])
         state = load_medium_state()
         assert state['download_queue'][hostname] == [simplepkg]
+
+# Test queuing downloads for a different system
+def test_queue_indirect(capsys, monkeypatch, hostname):
+    with init_cwd() as (retCode, initDir):
+        socket.sethostname(bogushostname)
+        args = ['install', '--target=%s' % (hostname), simplepkg]
+        monkeypatch.setattr('sys.stdin', StringIO('y'))
+        assert run(args) == 0
+        captured = capsys.readouterr()
+        output = captured.out.splitlines()
+        assert re.match("Need to download 1 packages totaling [0-9,]+ bytes", output[0])
+        assert re.match("Add to download queue\\?", output[1])
+        assert re.match("Queued fonts-3270 for download", output[2])
+        state = load_medium_state()
+        assert state['download_queue'][hostname] == [simplepkg]
+        assert bogushostname not in state['download_queue']
+        assert bogushostname not in state['install_queue']
+        socket.sethostname(hostname)
 
 # Test show details
 def test_queue_show_details(capsys, monkeypatch):
