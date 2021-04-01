35cdba8e4305645d2afeb3f053c331aceef42ed2
Riley Baxter <rbrileybaxter@gmail.com>
2021-04-01 18:22:33 -0400

    Cleanup queue download tests, add multiple package test

--- tests/test_0002-queue_download.py
+++ tests/test_0002-queue_download.py
@@ -31,7 +31,7 @@ def test_queue_direct(capsys, monkeypatch, hostname):
         output = captured.out.splitlines()
         assert re.match("Need to download 1 packages totaling [0-9,]+ bytes", output[0])
         assert re.match("Add to download queue\\?", output[1])
-        assert re.match("Queued fonts-3270 for download", output[2])
+        assert re.match("Queued %s for download" % (simplepkg), output[2])
         state = load_medium_state()
         assert state['download_queue'][hostname] == [simplepkg]
 
@@ -46,7 +46,7 @@ def test_queue_indirect(capsys, monkeypatch, hostname):
         output = captured.out.splitlines()
         assert re.match("Need to download 1 packages totaling [0-9,]+ bytes", output[0])
         assert re.match("Add to download queue\\?", output[1])
-        assert re.match("Queued fonts-3270 for download", output[2])
+        assert re.match("Queued %s for download" % (simplepkg), output[2])
         state = load_medium_state()
         assert state['download_queue'][hostname] == [simplepkg]
         assert bogushostname not in state['download_queue']
@@ -60,7 +60,7 @@ def test_queue_show_details(capsys, monkeypatch):
         monkeypatch.setattr('sys.stdin', StringIO('sn'))
         assert run(args) == 0
         captured = capsys.readouterr()
-        assert re.search("The following NEW packages will be installed:\n  fonts-3270\n0 upgraded, 1 newly installed", captured.out)
+        assert re.search("The following NEW packages will be installed:\n  %s\n0 upgraded, 1 newly installed" % (simplepkg), captured.out)
 
 # Test print URIs
 def test_queue_print_uri(capsys, monkeypatch):
@@ -69,7 +69,7 @@ def test_queue_print_uri(capsys, monkeypatch):
         monkeypatch.setattr('sys.stdin', StringIO('pn'))
         assert run(args) == 0
         captured = capsys.readouterr()
-        assert re.search("'http:\\/\\/.*fonts-3270.*\\.deb' fonts-3270.*\\.deb [0-9]+", captured.out)
+        assert re.search("'http:\\/\\/.*%s.*\\.deb' %s.*\\.deb [0-9]+" % (simplepkg, simplepkg), captured.out)
 
 # Test queuing package with dependencies
 def test_queue_with_deps(capsys, monkeypatch, hostname):
@@ -81,6 +81,21 @@ def test_queue_with_deps(capsys, monkeypatch, hostname):
         output = captured.out.splitlines()
         assert re.match("Need to download [0-9]+ packages totaling [0-9,]+ bytes", output[0])
         assert re.match("Add to download queue\\?", output[1])
-        assert re.match("Queued wireshark for download", output[2])
+        assert re.match("Queued %s for download" % complexpkg, output[2])
         state = load_medium_state()
         assert state['download_queue'][hostname] == [complexpkg]
+
+# Test queuing multiple packages
+def test_queue_multiple(capsys, monkeypatch, hostname):
+    with init_cwd() as (retCode, initDir):
+        args = ['install', simplepkg, complexpkg]
+        monkeypatch.setattr('sys.stdin', StringIO('y'))
+        assert run(args) == 0
+        captured = capsys.readouterr()
+        output = captured.out.splitlines()
+        assert re.match("Need to download [0-9]+ packages totaling [0-9,]+ bytes", output[0])
+        assert re.match("Add to download queue\\?", output[1])
+        assert re.match("Queued %s for download" % simplepkg, output[2])
+        assert re.match("Queued %s for download" % complexpkg, output[3])
+        state = load_medium_state()
+        assert state['download_queue'][hostname] == [simplepkg,complexpkg]
